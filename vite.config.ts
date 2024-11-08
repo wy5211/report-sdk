2c420498999f4641b0e6987c30aff426686e3eab
wangtao <1153695045@qq.com>
2024-11-08 15:47:56 +0800

    feat: 更新打包方式，不产生公共模块
    原因： 微信不支持公共包引入，只能有一个index.js

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,26 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    dts({
-      rollupTypes: true,
-      insertTypesEntry: true
-    })
-  ],
-  build: {
-    lib: {
-      name: 'xm-web-sdk',
-      entry: [
-        'src/index.rn.ts',
-        'src/index.wx.ts',
-        'src/index.pc.ts',
-      ],
-    },
-    rollupOptions: {
-      external: 'react-native'
-    }
-  }
+  // plugins: [
+  //   tsconfigPaths(),
+  //   // dts({
+  //   //   rollupTypes: true,
+  //   // })
+  // ],
+  // build: {
+  //   lib: {
+  //     name: 'xm-web-sdk',
+  //     entry: [
+  //       // 'src/index.rn.ts',
+  //       'src/index.wx.ts',
+  //       // 'src/index.pc.ts',
+  //     ],
+  //   },
+  //   rollupOptions: {
+  //     external: 'react-native',
+  //     output: {
+  //       manualChunks: {}
+  //     }
+  //   }
+  // }
 })
